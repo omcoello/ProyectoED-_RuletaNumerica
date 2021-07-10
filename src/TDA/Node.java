7bfa3bf1c24b9d78c0cf9f1ca73501bec3dfdb5d
Omar <omcoello@espol.edu.ec>
2021-07-10 14:10:26 -0500

    Clase Node

--- src/TDA/Node.java
+++ src/TDA/Node.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package TDA;
 
 /**
  *
  * @author omarc
  */
-public class Node {
+public class Node <E> {
+    
+    E content;
+    Node<E> next;
+    Node<E> previous;
+    
+    public Node (){
+        content = null;
+        this.next = null;
+        this.previous = null;
+    }
+    
+    public Node(E content){
+        this.content = content;
+        this.next = null;
+        this.previous = null;
+    }
+
+    public E getContent() {
+        return content;
+    }
+
+    public void setContent(E content) {
+        this.content = content;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node<E> previous) {
+        this.previous = previous;
+    }
+                        
+    
+    
     
 }
