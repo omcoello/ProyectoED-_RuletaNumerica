a17728a53b2f786aa911d749c9278eaecf55501e
Alexis Loor <caalloor@espol.edu.ec>
2021-07-16 13:37:20 -0500

    funcion adicional: decisiones de maquina

--- src/PaneView/Main.java
+++ src/PaneView/Main.java
@@ -10,6 +10,7 @@ import Ruleta.CirculoNumerico;
 import Ruleta.RuletaNum;
 import TDA.CircularlyDoubleLinkedList;
 import java.util.LinkedHashSet;
+import java.util.Random;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.scene.Scene;
@@ -26,6 +27,7 @@ public class Main extends Application {
     boolean perdedor = false;
     int numeroProhibido = 20;
     RuletaNum ruletaNumerica;
+    double resX = 500, resY = 500;
     
     public static void main(String[] arg) {
 //        for (int i = 1; i < 5; ++i) {
@@ -37,7 +39,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        double resX = 500, resY = 500;
+        //double resX = 500, resY = 500;
         
         LinkedHashSet<CirculoNumerico> rul = new LinkedHashSet<>();
         CirculoNumerico cn1 = new CirculoNumerico(new CircularlyDoubleLinkedList());
@@ -78,6 +80,52 @@ public class Main extends Application {
     
     }
     
-    
+    public void movimientoMaquina(){
+        Random random = new Random();
+        
+        //indice random para remover un elemento o rotar una lista circular
+        int index1 = random.nextInt(2);
+        // index1 = 0 para rotar
+        // index1 = 1 para eliminar
+        
+        //indice random para obtener una de las listas (para rotacion)
+        int bound = ruletaNumerica.getRuletasNumericas().size();
+        int index2 = random.nextInt(bound);
+        
+        //indice random para aplicar el remove() en caso de eliminacion
+        int indexRemove;
+        
+        // variable boolean de control para el sentido de las rotaciones
+        boolean rotacion = random.nextBoolean();
+        // rotacion = true para rotar a la derecha
+        // rotacion = false para rotar a la izquierda
+
+        int control = 0;
+        for(CirculoNumerico cn: ruletaNumerica.getRuletasNumericas()){
+            if(index1 == 0){
+                while (control != index2){
+                    control++;
+                }
+                if(control == index2){
+                    if(rotacion){
+                        cn.rotarDerecha();
+                        System.out.println("Se ha rotado a la derecha");
+                    } else{
+                        cn.rotarIzquierda();
+                        System.out.println("Se ha rotado a la izquierda");
+                    }
+                }
+
+
+            } else if(index1 == 1){
+                indexRemove = random.nextInt(cn.getListaNumerica().size());
+                cn.getListaNumerica().remove(indexRemove);
+                System.out.println("Se hizo una eliminacion");
+            }
+        }
+        
+        new RuletaController().generateRuleta(ruletaNumerica, resX, pane);
+        
+    }
     
 }
