73f02c78754b541d7766bfcb66a9ad433028f74a
David <dagavint@espol.edu.ec>
2021-07-17 17:14:27 -0500

    fuentes

--- src/PaneView/PaneManagement.java
+++ src/PaneView/PaneManagement.java
@@ -55,7 +55,7 @@ public class PaneManagement {
     public VBox getIniRoot() {
         iniRoot = new VBox(17);
 
-        String style = "-fx-font-weight: bold; -fx-font-size: 16px;-fx-background-color: #44BEC6;";
+        String style = "-fx-font-weight: bold; -fx-font-size: 20px;-fx-background-color: #44BEC6;";
 
         HBox circleHb = new HBox(20);
         Label circleLabel = new Label("Circulos: ");
@@ -86,7 +86,7 @@ public class PaneManagement {
         compDecition.setStyle(style);
 
         Button playButton = new Button("Jugar");
-        playButton.setStyle("-fx-font-weight: bold; -fx-font-size: 16px;");
+        playButton.setStyle("-fx-font-weight: bold; -fx-font-size: 20px;");
         playButton.setOnAction(e -> {
 
             String bet = betText.getText().trim();
@@ -153,14 +153,20 @@ public class PaneManagement {
         rc.generateRuleta(rn, 650, gameRoot);
 
         VBox vb = new VBox(15);
+        vb.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
+        + "-fx-border-width: 2;" + "-fx-border-insets: 5;"
+        + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
         RadioButton rotateRb = new RadioButton("Rotacion");
         rotateRb.setUserData("R");
         rotateRb.setSelected(true);
+        rotateRb.setStyle("-fx-font-weight: bold; -fx-font-size:18px;");
 
         RadioButton eliminateRb = new RadioButton("Eliminacion");
+        eliminateRb.setStyle("-fx-font-weight: bold; -fx-font-size:18px;");
         eliminateRb.setUserData("E");
 
         Label orientationLb = new Label("Direccion de giro:");
+        orientationLb.setStyle("-fx-font-weight: bold; -fx-font-size:18px;");
         cb = new ComboBox();
         cb.getItems().addAll("Izquierda", "Derecha");
         cb.setValue(cb.getItems().get(0));
@@ -195,14 +201,24 @@ public class PaneManagement {
         }
 
         HBox valueHb = new HBox(10);
+        valueHb.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
+        + "-fx-border-width: 2;" + "-fx-border-insets: 5;"
+        + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
         Label valueTitle = new Label("Valor: ");
+        valueTitle.setStyle("-fx-font-weight: bold; -fx-font-size:18px;");
         Label valueNumber = new Label(String.valueOf(rn.calcularValorRuleta()));
+        valueNumber.setStyle("-fx-font-weight: bold; -fx-font-size:18px;");
         valueHb.getChildren().addAll(valueTitle, valueNumber);
         vb.getChildren().add(valueHb);
 
         HBox betHb = new HBox(10);
+        betHb.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
+        + "-fx-border-width: 2;" + "-fx-border-insets: 5;"
+        + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
         Label betTitle = new Label("Apuesta: ");
+        betTitle.setStyle("-fx-font-weight: bold; -fx-font-size:18px;");
         Label betNumber = new Label(betText.getText());
+        betNumber.setStyle("-fx-font-weight: bold; -fx-font-size:18px;");
         betHb.getChildren().addAll(betTitle, betNumber);
         vb.getChildren().add(betHb);
 
